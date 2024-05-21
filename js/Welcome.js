a02c6f265efa870a93d1109c49649c29a661c19d
Marco Zagaria <marcozagaria@live.com>
2024-05-21 10:43:48 +0200

    fine pagina welcome

--- js/Welcome.js
+++ js/Welcome.js
@@ -1,1 +1,11 @@
-const x = document.getElementById("myCheck").required;
+const checkbox = document.getElementById('myCheck');
+    const button = document.getElementById('myButton');
+
+    checkbox.addEventListener('change', () => {
+        button.disabled = !checkbox.checked;
+    });
+    
+
+    button.addEventListener('click', () => {
+        window.location.href = "/HTML/Quiz.html";
+    });
